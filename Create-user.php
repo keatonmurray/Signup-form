666538ef475c48f1b0394c3ce9b0a100835c3589
keatonmurray <112250794+keatonmurray@users.noreply.github.com>
2023-03-13 03:19:58 -0500

    Update Create-user.php
    Added error messages for fields that are left empty by the user

--- Create-user.php
+++ Create-user.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
 
+<?php 
+	include_once "Includes/Validator-object.php";
+?>
+
 <html>
 	<head>
 		<title>Create an account</title>
-		<link rel="stylesheet" href="Public/Styles.css">
+		<link rel="stylesheet" href="Public/Style.css">
 	</head>
 	<body id="index">
         <form name="signup" action="" method="POST">
             <h2>SIGN UP</h2><br>
             <label class="uname">Name</label><br>
-            <input class="input" type="text" name="name"><br><br>
+            <input class="input" type="text" name="name"><br>
+	         <small>
+	         	<?php echo $name ?? '' ?>
+	         </small><br><br>
             <label class="uname">Email</label><br>
-            <input class="input" type="email" name="email"><br><br>
+            <input class="input" type="email" name="email"><br>
+           	<small>
+           		<?php echo $email ?? '' ?>
+           	</small><br><br>
             <label class="uname">Username</label><br>
-            <input class="input" type=text name ="username"><br><br>
+            <input class="input" type=text name ="username"><br>
+            <small>
+            	<?php echo $username ?? '' ?>
+        	</small><br><br>
             <label class="pass">Password</label><br>
-            <input class="input" type="password" name="password"><br><br>
+            <input class="input" type="password" name="password"><br>
+            <small>
+            	<?php echo $password ?? '' ?>
+        	</small><br><br>
             <p><a href="">Already have an account?</a></p>
             <button class="btn" type="submit" name="submit">Signup</button>
             <button class="btn" type="submit" name="back"><a href="">Cancel</a></button>
