66113e456aab3ca1c1f733bbd17bb7e9022404e4
keatonmurray <112250794+keatonmurray@users.noreply.github.com>
2023-03-13 02:15:54 -0500

    Update Validator.php
    Removed the instance creation, and moved it to the includes folder

--- Classes/Validator.php
+++ Classes/Validator.php
@@ -17,13 +17,3 @@
 			}
 		}
 	}
-
-//instantiate the class
-
-if (isset($_POST['submit'])) {
-
-	$fields = [$_POST['name'], $_POST['email'], $_POST['username'], $_POST['password']];
-
-	$validate = new Validator($fields);
-	$validate->validate();
-}
