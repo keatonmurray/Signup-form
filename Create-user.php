af6dad2be29846bb532bca35370a6cf9d4260693
keatonmurray <112250794+keatonmurray@users.noreply.github.com>
2023-03-13 01:45:30 -0500

    Added the frontend file
    File contains html codes to build the frontend of the site

---
+++ Create-user.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Create an account</title>
+		<link rel="stylesheet" href="Public/Styles.css">
+	</head>
+	<body id="index">
+        <form name="signup" action="" method="POST">
+            <h2>SIGN UP</h2><br>
+            <label class="uname">Name</label><br>
+            <input class="input" type="text" name="name"><br><br>
+            <label class="uname">Email</label><br>
+            <input class="input" type="email" name="email"><br><br>
+            <label class="uname">Username</label><br>
+            <input class="input" type=text name ="username"><br><br>
+            <label class="pass">Password</label><br>
+            <input class="input" type="password" name="password"><br><br>
+            <p><a href="">Already have an account?</a></p>
+            <button class="btn" type="submit" name="submit">Signup</button>
+            <button class="btn" type="submit" name="back"><a href="">Cancel</a></button>
+        </form>
+    </body>
+</html>
